fe09782cc74e15c59c11022637edb5c892fc7d58
Bino Jp <binojp2005@gmail.com>
2025-12-28 14:04:32 +0530

    Initial commit

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/User");
 const Incident = require("../models/Incident");
 const jwt = require("jsonwebtoken");
 
